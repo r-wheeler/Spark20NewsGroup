3a9d9ede6920063887858a760a1b9bcaf5627477
ryan <ryanmwheeler@gmail.com>
2014-12-26 14:02:22 -0500

    moved nlp to seperate class

---
+++ src/main/scala/com/brokendata/NLPUtils.scala
@@ -0,0 +1,54 @@
+package com.brokendata
+
+import java.util.Properties
+
+import edu.stanford.nlp.ling.CoreAnnotations.{LemmaAnnotation, TokensAnnotation, SentencesAnnotation}
+import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.JavaConversions._
+
+
+object NLPUtils {
+
+  def tokenizeAndStem(text: String, stopWords: Set[String] ): Seq[String] = {
+    val props = new Properties()
+    props.put("annotators", "tokenize, ssplit, pos, lemma")
+
+    val pipeline = new StanfordCoreNLP(props)
+    val doc = new Annotation(text)
+
+    pipeline.annotate(doc)
+
+    val lemmas = new ArrayBuffer[String]()
+    val sentences = doc.get(classOf[SentencesAnnotation])
+    for (sentence <- sentences;
+         token <- sentence.get(classOf[TokensAnnotation])) {
+      val lemma = token.get(classOf[LemmaAnnotation])
+      if (lemma.length > 2 && !stopWords.contains(lemma)
+        && isOnlyLetters(lemma)) {
+        lemmas += lemma.toLowerCase
+      }
+    }
+
+    lemmas
+  }
+
+  def loadStopWords(path: String) = scala.io.Source.fromFile(path).getLines().toSet
+
+  def isOnlyLetters(str: String): Boolean = {
+    // While loop for high performance
+    var i = 0
+    while (i < str.length) {
+      if (!Character.isLetter(str.charAt(i))) {
+        return false
+      }
+      i += 1
+    }
+    true
+  }
+
+
+
+
+}
--- src/main/scala/com/brokendata/NewsGroup.scala
+++ src/main/scala/com/brokendata/NewsGroup.scala
@@ -1,9 +1,12 @@
 package com.brokendata
 
 import com.brokendata.Utils._
-/**
- * Created by ryan on 12/23/14.
- */
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.SparkContext._
+import org.apache.spark.mllib.linalg._
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+import org.apache.spark.rdd.RDD
+
 object NewsGroup {
 
   def main(args: Array[String]) = {
@@ -14,6 +17,11 @@ object NewsGroup {
     val stopWords = sc.broadcast(loadStopWords("stopwords.txt")).value
 
     */
+    val conf = new SparkConf().setAppName("SimpleTextClassificationPipeline")
+    val sc = new SparkContext(conf)
+
+
+
 
   }
 
