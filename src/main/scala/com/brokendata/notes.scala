091867f2f49e16d75f3c28d8baac231ec2bb394d
ryan <ryanmwheeler@gmail.com>
2014-12-28 21:12:59 -0500

    fix path

--- src/main/scala/com/brokendata/notes.scala
+++ src/main/scala/com/brokendata/notes.scala
@@ -18,9 +18,9 @@ object notes {
   The TFIDF vectorizor expects the data to be a RDD[Vector], when attempting to fit the idf model where the data was
   RDD[Int, Vector], calling idf.transform(RDD[INT, Vector]._2) would fail. digging around showed that this generally agreed as not good
   with calls for a PR to fix it. A work around in to create 2 copies of the data:
-    - 1 data set consisting of LabledPoint(numericLabel, Vector)
+    - 1 data set consisting of LabeledPoint(numericLabel, Vector)
     - another dataset that is just the vectors
-    - then zip the 2 together and return a new labled point.
+    - then zip the 2 together and return a new labeled point.
 
 
    */
